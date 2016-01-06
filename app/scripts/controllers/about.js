4c5ab02278e8e28490e304e6bc082872aa31f4a4
mkreiser <mkreise2@illinois.edu>
2016-01-06 16:44:54 -0500

    Add Angular generated code

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name twitterBoardApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the twitterBoardApp
+ */
+angular.module('twitterBoardApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name twitterBoardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the twitterBoardApp
+ */
+angular.module('twitterBoardApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
